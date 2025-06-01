14ef2bd5c251ae31db5f9382a08554064ce88969
Ganesh Asimshetti <ganeshweb63@gmail.com>
2025-06-01 21:44:03 +0530

    Updated edge cases in second largest number.

--- src/warmUp/secondLargestNumber.js
+++ src/warmUp/secondLargestNumber.js
@@ -1,13 +1,16 @@
-const arr = [3, 5, 1, 66, 33, 77, 78, 11, 56];
+const arr = [3, 5, 1, 66, 33, 77, 78, 78, 11, 56];
 
 function findSecondLargestNumber(arr) {
+  if (arr.length < 2) {
+    return "Array should have min 2 elements";
+  }
   let firstLargestNumber = -Infinity;
   let secondLargestNumber = -Infinity;
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] > firstLargestNumber) {
       secondLargestNumber = firstLargestNumber;
       firstLargestNumber = arr[i];
-    } else if (arr[i] > secondLargestNumber) {
+    } else if (arr[i] > secondLargestNumber && arr[i] != firstLargestNumber) {
       secondLargestNumber = arr[i];
     }
   }
